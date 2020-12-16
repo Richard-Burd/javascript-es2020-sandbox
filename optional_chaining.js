397774121c8581de47bf226609de8835caac4ec5
Richard-Burd <qalbalassadalasfour@gmail.com>
2020-12-16 15:36:37 -0800

    optional chaining refactored

--- optional_chaining.js
+++ optional_chaining.js
@@ -1,5 +1,3 @@
-// Available online here: https://codepen.io/richardburd/pen/gOwWRNO?editors=0012
-
 // https://www.youtube.com/watch?v=jdU5X3GJXBs
 // ?. JavaScript 2020's Optional Chaining by Better Dev
 
@@ -13,47 +11,50 @@ const animals = {
 // this will throw an error
 
 // this one does exist:
-const name1 = animals.dog?.name
+console.log(animals.dog?.name)
+//=> "Bella"
 
 // this one does not exist:
-const name2 = animals.duck?.name
+console.log(animals.duck?.name)
+//=> undefined
 
-// this one does not exist, and has a default value:
-const name3 = animals.goose?.name ?? 'Nick Bradshaw'
+// this one does not exist, but it has a default value:
+console.log(animals.goose?.name ?? 'Nick Bradshaw')
+//=> "Nick Bradshaw"
 
-// You can even do multiple chaining on an object:
-const name4 = animals.class?.infraclass?.order ?? 'Diprotodontia'
+// You can also do multiple chaining on an object:
+console.log(animals.class?.infraclass?.order ?? 'Diprotodontia')
+//=> "Diprotodontia"
 
 // ...but you cannot do optional chaining on a top level parent:
-// const name5 = kingdom.class?.infraclass?.order ?? 'Diprotodontia'
+// console.log(kingdom.class?.infraclass?.order ?? 'Diprotodontia')
 // ...this will throw an error because no "kingdom" exists anywhere
+//=> ReferenceError: kingdom is not defined
 
 // Optional chaining works with arrays:
 const lockbox = {chamber: [{code: '1'}, {code: '2'}, {code: '3'}]};
-const secretkey = lockbox.chamber?.[0].code
-
-console.log(name1);
-console.log(name2);
-console.log(name3);
-console.log(name4);
-console.log(secretkey);
+console.log(lockbox.chamber?.[0].code)
+//=> "1"
 
 // We can check to see if a method exists before calling it:
 const critters = {
   mice: [{name: 'Mickey'}, {name: 'Mini'}],
-  trap: function(){
+  trap: function(){ // anonymous function, immediately invoked
     console.log("a mouse has been caught!")
   },
-  more_specific_trap: function(name){
+  more_specific_trap: function(name){ // anonymous function, immediately invoked
     console.log(`${name} has been caught!`)
   }
 }
 
 // this method does exist:
 const trap1 = critters.trap?.();
+//=> "a mouse has been caught!"
 
 // this method also exists, and so does the variable passed into it:
 const trap2 = critters.more_specific_trap?.(critters.mice[0].name)
+//=> "Mickey has been caught!"
 
-// this method does exist, but not the variable passed into it:
+// this method does exist, but the variable passed into it does not exist:
 const trap3 = critters.more_specific_trap?.(critters.mice[8]?.name ?? 'Speedy')
+//=> "Speedy has been caught!"
